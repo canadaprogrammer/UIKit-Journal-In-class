4305e5b6c8afb1ef0db991b1fc1495c7f7c634d5
jmbae <jmbae@codegrove.co.kr>
2024-05-09 14:52:02 +0900

    TableView Outlet

--- JRNL/JournalListViewController.swift
+++ JRNL/JournalListViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class JournalListViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
