131ca8d1d2958318307c09057cac543d6518cbfb
jmbae <jmbae@codegrove.co.kr>
2024-05-10 11:27:50 +0900

    SampleData

--- JRNL/Journal List Screen/Model/JournalEntry.swift
+++ JRNL/Journal List Screen/Model/JournalEntry.swift
@@ -17,7 +17,8 @@ class JournalEntry {
     let latitude: Double?
     let longitude: Double?
     
-    init?(date: Date, rating: Int, title: String, body: String,
+    // MARK: - Intialization
+    init?(rating: Int, title: String, body: String,
           photo: UIImage? = nil, latitude: Double? = nil, longitude: Double? = nil) {
         if title.isEmpty || body.isEmpty || rating < 0 || rating > 5 {
             return nil
@@ -29,5 +30,31 @@ class JournalEntry {
         self.photo = photo
         self.latitude = latitude
         self.longitude = longitude
+    }
+}
+
+// MARK: - Sample data
+struct SampleJournalEntryData {
+    var journalEntries: [JournalEntry] = []
+    
+    mutating func createSampleJournalEntryData() {
+        let photo1 = UIImage(systemName: "sun.max")
+        let photo2 = UIImage(systemName: "cloud")
+        let photo3 = UIImage(systemName: "cloud.sun")
+        guard let journalEntry1 = JournalEntry(rating: 5, title: "Good",
+                                               body: "Today is good day", photo: photo1) else {
+            fatalError("Unable to instantiate journalEntry1")
+        }
+        guard let journalEntry2 = JournalEntry(rating: 0, title: "Bad",
+                                               body: "Today is bad day", photo: photo2) else {
+            fatalError("Unable to instantiate journalEntry2")
+        }
+        guard let journalEntry3 = JournalEntry(rating: 3, title: "Ok",
+                                               body: "Today is Ok day", photo: photo1) else {
+            fatalError("Unable to instantiate journalEntry3")
+        }
+        
+        journalEntries += [journalEntry1, journalEntry2, journalEntry3]
+        
     }
 }
