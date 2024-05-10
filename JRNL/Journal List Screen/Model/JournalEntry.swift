e5244eaed188d25aa94935eab712c16dbd57bfd0
jmbae <jmbae@codegrove.co.kr>
2024-05-10 11:15:44 +0900

    JournalEntry Model Class

---
+++ JRNL/Journal List Screen/Model/JournalEntry.swift
@@ -0,0 +1,33 @@
+//
+//  JournalEntry.swift
+//  JRNL
+//
+//  Created by Jungman Bae on 5/10/24.
+//
+
+import UIKit
+
+class JournalEntry {
+    // MARK: - Properties
+    let date: Date
+    let rating: Int
+    let entryTitle: String
+    let entryBody: String
+    let photo: UIImage?
+    let latitude: Double?
+    let longitude: Double?
+    
+    init?(date: Date, rating: Int, title: String, body: String,
+          photo: UIImage? = nil, latitude: Double? = nil, longitude: Double? = nil) {
+        if title.isEmpty || body.isEmpty || rating < 0 || rating > 5 {
+            return nil
+        }
+        self.date = Date()
+        self.rating = rating
+        self.entryTitle = title
+        self.entryBody = body
+        self.photo = photo
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
