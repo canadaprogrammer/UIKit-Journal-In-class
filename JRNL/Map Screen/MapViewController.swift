a2752d5f622543a9c565e1c608632627c7835fd7
Jin Park <jinpark1504@gmail.com>
2024-05-14 16:51:55 +0900

    connect mapview and display snapshot

--- JRNL/Map Screen/MapViewController.swift
+++ JRNL/Map Screen/MapViewController.swift
@@ -14,6 +14,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     @IBOutlet var mapView: MKMapView!
     let locationManager = CLLocationManager()
     var sampleJournalEntryData = SampleJournalEntryData()
+    var selectedJournalEntry: JournalEntry?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +44,45 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
         print("Failed to find user's location: \(error.localizedDescription)")
     }
+    
+    // MARK: - MKMapViewDelegate
+    func mapView(_ mapView: MKMapView, viewFor annotation: any MKAnnotation) -> MKAnnotationView? {
+        let identifier = "mapAnnotation"
+        if annotation is JournalEntry {
+            if let annotationview = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) {
+                annotationview.annotation = annotation
+                return annotationview
+            } else {
+                let annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                annotationView.canShowCallout = true
+                let callloutButton = UIButton(type: .detailDisclosure)
+                annotationView.rightCalloutAccessoryView = callloutButton
+                return annotationView
+            }
+        }
+        return nil
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard let annotation = mapView.selectedAnnotations.first else {
+            return
+        }
+        selectedJournalEntry = annotation as? JournalEntry
+        self.performSegue(withIdentifier: "showMapDetail", sender: self)
+    }
+    
+    // MARK: - navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard segue.identifier == "showMapDetail" else {
+            fatalError("Unexpected segue identifier")
+        }
+        guard let entryDetailViewController = segue.destination as? JournalEntryDetailViewController else {
+            fatalError("Unexpected view controller")
+        }
+        entryDetailViewController.selectedJournalEntry = selectedJournalEntry
+    }
+    
     // MARK: - Methods
     func setInitialRegion(lat: CLLocationDegrees, long: CLLocationDegrees) -> MKCoordinateRegion {
         MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: long), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
