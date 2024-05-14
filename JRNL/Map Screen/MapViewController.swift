af423a08094b57cf09a99db4e200f7245f13ae7e
Jin Park <jinpark1504@gmail.com>
2024-05-14 14:35:15 +0900

    Map view controller connect

---
+++ JRNL/Map Screen/MapViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MapViewController.swift
+//  JRNL
+//
+//  Created by Jungjin Park on 2024-05-14.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        self.navigationItem.title = "Loading..."
+        locationManager.requestLocation()
+    }
+    
+    // MARK: - CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let myLocation = locations.first {
+            let lat = myLocation.coordinate.latitude
+            let long = myLocation.coordinate.longitude
+            self.navigationItem.title = "Map"
+            mapView.region = setInitialRegion(lat: lat, long: long)
+            
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
+    // MARK: - Methods
+    func setInitialRegion(lat: CLLocationDegrees, long: CLLocationDegrees) -> MKCoordinateRegion {
+        MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: long), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    }
+}
